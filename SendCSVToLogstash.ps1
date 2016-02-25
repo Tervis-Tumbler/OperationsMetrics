01ab2f3be56e3f48bf2fff48e9f2374b6a8db675
ChrisMagnuson <hourglasssand@gmail.com>
2016-02-24 22:10:52 -0500

    Initial commit

---
+++ SendCSVToLogstash.ps1
@@ -0,0 +1,16 @@
+﻿#POC
+#Invoke-WebRequest -Uri http://localhost:8080 -ContentType "application/json" -Body '{"thing":"value", "thing2":"Value2"}' -Method Post
+function Send-CSVToLogstash {
+    param(
+        $CSVFilePath
+    )
+    $CSVFileRecords = Import-Csv -Delimiter ',' -Path $CSVFilePath
+    $CSVFileRecords | % {
+        $progressPreference = 'silentlyContinue'
+        $(Invoke-WebRequest -Uri http://localhost:8080 -ContentType "application/json" -Method Post -Body $($_ | ConvertTo-Json) )| Out-Null
+        $progressPreference = 'Continue'
+    }
+}
+
+#Send-CSVToLogstash -CSVFilePath "C:\Users\cmagnuson\Desktop\Operations reporting\DC Report 2016.01.29 0500 DC Light sample.csv"
+Send-CSVToLogstash -CSVFilePath "C:\Users\cmagnuson\Desktop\Operations reporting\DC Report 2016.01.29 0500 DC Light.csv"
